ab4bf3c4929d9cf0186272b05ebcbf546caa32c8
jochen <jochen.heckl@gmail.com>
2023-12-10 13:23:23 +0100

    Adds workflows

---
+++ Assets/Samples/DataBinding/2.1.0-rc01/MyFirstDataBoundView (Getting Started Tutorial)/Scripts/CubeViewDataSource.cs
@@ -0,0 +1,12 @@
+using System;
+
+using UnityEngine;
+
+namespace de.JochenHeckl.Unity.DataBinding.Examples.GettingStarted
+{
+    public class CubeViewDataSource : DataSourceBase<CubeViewDataSource>
+    {
+        public Vector3 CubeScale { get; set; } = Vector3.one;
+        public Color CubeColor { get; set; } = Color.grey;
+    }
+}
